78ee7975ccb959a130ad4afb68646e7bef94238f
bahaehmimdi <48775632+bahaehmimdi@users.noreply.github.com>
2023-08-18 13:05:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,13 @@ from flask_socketio import SocketIO, emit
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/caller')
+def caller():
+    return render_template('caller.html')
+
+@app.route('/receiver')
+def receiver():
+    return render_template('receiver.html')
 
 @socketio.on('signal')
 def handle_signal(data):
